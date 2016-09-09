9bde9229ae82652f3191373616527eee970e221a
TheDominator54 <Rand.dominic@gmail.com>
2016-09-09 12:59:56 -0700

    Update Big_Garage_Stable_Accessory.js

--- Big_Garage_Stable_Accessory.js
+++ Big_Garage_Stable_Accessory.js
@@ -5,153 +5,91 @@ var mqtt = require('mqtt');
 var options = {
   port: 1883,
   host: '192.168.1.190',
-  clientId: 'DomPi_Big_garage'
+  clientId: 'DomPi_Light'
 };
 var client = mqtt.connect(options);
-console.log("Big Garage Connected to MQTT broker");
+console.log("Light Connected to MQTT broker");
 
-// HomeKit types required
-var types = require("./types.js");
-var exports = module.exports = {};
+var Accessory = require('../').Accessory;
+var Service = require('../').Service;
+var Characteristic = require('../').Characteristic;
+var uuid = require('../').uuid;
 
-var execute = function(accessory,characteristic,value) {
-  console.log("executed accessory: " + accessory + ", and characteristic: " + characteristic + ", with value: " +  value + "."); 
-};
+// here's a fake hardware device that we'll expose to HomeKit
+var CHRISTMAS_LIGHT = {
+  powerOn: false,
+  
+  setPowerOn: function(on) { 
+    console.log("Turning the Christmas light %s!", on ? "on" : "off");
+    if (on) {
+      client.publish('inTopic1', '1');
+      CHRISTMAS_LIGHT.powerOn = on;
+   	}
+    else {
+	    client.publish('inTopic1','0');
+      CHRISTMAS_LIGHT.powerOn = false;
+   };
 
-exports.accessory = {
-  displayName: "Garage Door Opener",
-  username: "3C:5A:3D:EE:5E:FA",
-  pincode: "031-45-154",
-  services: [{
-    sType: types.ACCESSORY_INFORMATION_STYPE, 
-    characteristics: [{
-      cType: types.NAME_CTYPE, 
-      onUpdate: null,
-      perms: ["pr"],
-      format: "string",
-      initialValue: "Garage Door Opener",
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "Name of the accessory",
-      designedMaxLength: 255    
-    },{
-      cType: types.MANUFACTURER_CTYPE, 
-      onUpdate: null,
-      perms: ["pr"],
-      format: "string",
-      initialValue: "Oltica",
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "Manufacturer",
-      designedMaxLength: 255    
-    },{
-      cType: types.MODEL_CTYPE,
-      onUpdate: null,
-      perms: ["pr"],
-      format: "string",
-      initialValue: "Rev-1",
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "Model",
-      designedMaxLength: 255    
-    },{
-      cType: types.SERIAL_NUMBER_CTYPE, 
-      onUpdate: null,
-      perms: ["pr"],
-      format: "string",
-      initialValue: "A1S2NASF88EW",
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "SN",
-      designedMaxLength: 255    
-    },{
-      cType: types.IDENTIFY_CTYPE, 
-      onUpdate: null,
-      perms: ["pw"],
-      format: "bool",
-      initialValue: false,
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "Identify Accessory",
-      designedMaxLength: 1    
-    }]
-  },{
-    sType: types.GARAGE_DOOR_OPENER_STYPE, 
-    characteristics: [{
-      cType: types.NAME_CTYPE,
-      onUpdate: null,
-      perms: ["pr"],
-      format: "string",
-      initialValue: "Garage Door Opener Control",
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "Name of service",
-      designedMaxLength: 255   
-    },{
-      cType: types.CURRENT_DOOR_STATE_CTYPE,
-      onUpdate: function(value) { 
-        console.log("Change:",value); 
-        execute("Garage Door - current door state", "Current State", value); 
-        client.publish('inTopic1', 'current Toggle');
-      },
-      onRead: function(callback) {
-        console.log("Read:");
-        execute("Garage Door - current door state", "Current State", null);
-        callback(undefined); // only testing, we have no physical device to read from
-        client.publish('inTopic1', 'current State?');
-      },
-      perms: ["pr","ev"],
-      format: "int",
-      initialValue: 0,
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "BlaBla",
-      designedMinValue: 0,
-      designedMaxValue: 4,
-      designedMinStep: 1,
-      designedMaxLength: 1    
-    },{
-      cType: types.TARGET_DOORSTATE_CTYPE,
-      onUpdate: function(value) { 
-        console.log("Change:",value); 
-        execute("Garage Door - target door state", "Current State", value); 
-        client.publish('inTopic1', 'target Toggle');
-      },
-      onRead: function(callback) {
-        console.log("Read:");
-        execute("Garage Door - target door state", "Current State", null);
-        callback(undefined); // only testing, we have no physical device to read from
-        client.publish('inTopic1', 'target State?');
-      },
-      perms: ["pr","pw","ev"],
-      format: "int",
-      initialValue: 0,
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "BlaBla",
-      designedMinValue: 0,
-      designedMaxValue: 1,
-      designedMinStep: 1,
-      designedMaxLength: 1    
-    },{
-      cType: types.OBSTRUCTION_DETECTED_CTYPE,
-      onUpdate: function(value) { 
-        console.log("Change:",value); 
-        execute("Garage Door - obstruction detected", "Current State", value); 
-        client.publish('inTopic1', 'obstruction Toggle');
-      },
-      onRead: function(callback) {
-        console.log("Read:");
-        execute("Garage Door - obstruction detected", "Current State", null);
-        callback(undefined); // only testing, we have no physical device to read from
-        client.publish('inTopic1', 'Obstruction State?');
-      },
-      perms: ["pr","ev"],
-      format: "bool",
-      initialValue: false,
-      supportEvents: false,
-      supportBonjour: false,
-      manfDescription: "BlaBla"
-    }]
-  }]
+  },
+  identify: function() {
+    console.log("Identify the light!");
+  }
 }
+
+// Generate a consistent UUID for our light Accessory that will remain the same even when
+// restarting our server. We use the `uuid.generate` helper function to create a deterministic
+// UUID based on an arbitrary "namespace" and the word "Christmaslight".
+var lightUUID = uuid.generate('hap-nodejs:accessories:Christmaslight');
+
+// This is the Accessory that we'll return to HAP-NodeJS that represents our fake light.
+var light = exports.accessory = new Accessory('Light', lightUUID);
+
+// Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
+light.username = "2A:2B:3C:4D:5E:FF";
+light.pincode = "031-45-154";
+
+// set some basic properties (these values are arbitrary and setting them is optional)
+light
+  .getService(Service.AccessoryInformation)
+  .setCharacteristic(Characteristic.Manufacturer, "Oltica")
+  .setCharacteristic(Characteristic.Model, "Rev-1")
+  .setCharacteristic(Characteristic.SerialNumber, "A1S2NASF88EW");
+
+// listen for the "identify" event for this Accessory
+light.on('identify', function(paired, callback) {
+  CHRISTMAS_LIGHT.identify();
+  callback(); // success
+});
+
+// Add the actual Lightbulb Service and listen for change events from iOS.
+// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
+light
+  .addService(Service.Lightbulb, "Christmas Light") // services exposed to the user should have "names" like "Fake Light" for us
+  .getCharacteristic(Characteristic.On)
+  .on('set', function(value, callback) {
+    CHRISTMAS_LIGHT.setPowerOn(value);
+    callback(); // Our fake Light is synchronous - this value has been successfully set
+  });
+
+// We want to intercept requests for our current power state so we can query the hardware itself instead of
+// allowing HAP-NodeJS to return the cached Characteristic.value.
+light
+  .getService(Service.Lightbulb)
+  .getCharacteristic(Characteristic.On)
+  .on('get', function(callback) {
+    
+    // this event is emitted when you ask Siri directly whether your light is on or not. you might query
+    // the light hardware itself to find this out, then call the callback. But if you take longer than a
+    // few seconds to respond, Siri will give up.
+    
+    var err = null; // in case there were any problems
+    
+    if (CHRISTMAS_LIGHT.powerOn) {
+      console.log("Are we on? Yes.");
+      callback(err, true);
+    }
+    else {
+      console.log("Are we on? No.");
+      callback(err, false);
+    }
+  });
